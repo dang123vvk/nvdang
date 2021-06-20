4013114b070e335caacbc6513d1b4b8147eddea8
Dang Nguyen <dang123vvk@gmail.com>
2021-06-20 18:59:30 +0700

    Update sitmap for web app

--- pages/_app.js
+++ pages/_app.js
@@ -56,6 +56,11 @@ function MyApp(props) {
         <Head>
           <title>Nguyễn Văn Đặng </title>
           <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="Trang web của Nguyễn Văn Đặng" />
+          <meta property="og:title" content="Về tôi - Trang web của Nguyễn Văn Đặng" />
+          <meta property="og:description" content="Trang web của Nguyễn Văn Đặng" />
+          <meta property="og:url" content="https://nvdang.dev/about" />
+          <meta property="og:type" content="website" />
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
